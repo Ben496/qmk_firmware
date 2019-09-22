81e4116f24a1b2feaa93e3ad3414cd69888f7c11
Ben Firth <ben496dev@gmail.com>
2019-09-22 17:38:18 -0500

    Improving game mode

--- keyboards/planck/keymaps/ben/keymap.c
+++ keyboards/planck/keymaps/ben/keymap.c
@@ -19,6 +19,7 @@ enum custom_keycodes {
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
+#define QWERTY MO(_QWERTY)
 #define NUMPAD MO(_NUMPAD)
 #define GAME0 DF(_GAME0)
 #define GAME DF(_GAME)
@@ -148,7 +149,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,               KC_5,               KC_6,             KC_7,    KC_8,    KC_9,    KC_0,    KC_ESC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,               KC_T,               KC_Y,             KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,               KC_G,               KC_H,             KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LCTL, KC_Z,    KC_X,    KC_C,    MT(MOD_LALT, KC_V), LT(_LOWER, KC_SPC), LT(_RAISE, KC_B), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH
+    KC_LCTL, LGUI_T(KC_Z), KC_X, MT(MOD_LALT, KC_C) ,KC_SPC,LT(_LOWER, KC_V),   LT(_RAISE, KC_B), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH
 )
 
 };
